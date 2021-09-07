be37c77fe8503ecbbd72988ab0921e59d32ecb1a
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-09-07 16:19:04 -0300

    ajusted route to aupdate repository

--- src/index.js
+++ src/index.js
@@ -30,21 +30,17 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { likes, ...updatedRepository } = request.body;
 
-  repositoryIndex = repositories.findindex(
-    (repository) => repository.id === id
-  );
+  const repository = repositories.find((repository) => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  Object.assign(repository, updatedRepository);
 
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
